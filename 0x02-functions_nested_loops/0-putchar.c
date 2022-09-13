bdd052d19053aa7dc6741089cd3efed3508265be
AlirukuEmmanuel <alirukuemma@gmail.com>
2022-09-13 09:09:44 -0700

    fistr one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * main- prints _putchar as a massage.
- *
- * Return: Always 0 (seccess)
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
-	int str[] = {72, 111, 108, 114, 116, 111, 106};	
-	int count, sz;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz, count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar("\n");
 	return (0);
+}
